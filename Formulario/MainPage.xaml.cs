a1dbdc9a2b2308a37f759a05a02fa983cf1f94b3
Albert.Gruning <Alberto.Gruning@grupoeconat.com>
2025-01-06 17:27:17 -0300

    feat: add method to take photo

--- Formulario/MainPage.xaml.cs
+++ Formulario/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-// using Plugin.Media.Abstractions;
-// using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Plugin.Media;
+using System.Security.Cryptography;
+using System.Runtime.CompilerServices;
 
 //using Android.App;
 
@@ -10,6 +12,30 @@ namespace Formulario;
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	protected bool SetProperty<T>(
+		ref T backingStore, T value,
+		[CallerMemberName]string propertyName = "",
+		Action onChanged = null)
+	{
+		if (EqualityComparer<T>.Default.Equals(backingStore, value))
+			return false;
+
+		backingStore = value;
+		onChanged?.Invoke();
+		OnPropertyChanged(propertyName);
+		return true;
+	}
+	private string _facePhoto1 = "";
+	public string FacePhoto1 {
+		get { return _facePhoto1; }
+		set { SetProperty(ref _facePhoto1, value); }
+	}
+	private string _facePhotoPath1 = "";
+	public string PhotoPath
+	{
+		get { return _facePhotoPath1; }
+		set { SetProperty(ref _facePhotoPath1, value); }
+	}
 
 	public MainPage()
 	{
@@ -34,45 +60,65 @@ public partial class MainPage : ContentPage
 			try
            {
             //    //Plugin.Media.Abstractions.Location _location = await GetGeolocation();
-            //    await CrossMedia.Current.Initialize();
+               await CrossMedia.Current.Initialize();
 
-            //    if (!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
-            //    {
-            //        await _dialogService.ShowError("Camara no detectada", "Error", "OK", null);
-            //        return;
-            //    }
-            //    var photoName = "";
-            //    var varName = "";
+               if (!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
+               {
+					await DisplayAlert("Error", "Camara no detectada" , "Aceptar");
+                   //await _dialogService.ShowError("Camara no detectada", "Error", "OK", null);
+                   return;
+               }
+               var photoName = "";
+               var varName = "";
 
-            // 	photoName = $"PHOTO_{DateTime.Now.ToString()}";
+            	photoName = $"PHOTO_{DateTime.Now.ToString()}";
 
 
-            //    var pName = "";
-            //    var pdirectory = $"Face_{new Random().Next(99).ToString()}_{DateTime.Now.ToString("yyyyMMddHHmmss")}.jpg";
+               	var pName = "";
+            	var pdirectory = $"Face_{new Random().Next(99).ToString()}_{DateTime.Now.ToString("yyyyMMddHHmmss")}.jpg";
                
-            //    pdirectory = DateTime.Now.ToString("yyyyMMddHHmmss");
+                pdirectory = DateTime.Now.ToString("yyyyMMddHHmmss");
 
-            //    var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
-            //    {
-            //        Name = pName,
-            //        Directory = pdirectory,
-            //        CompressionQuality = 75,
-            //        CustomPhotoSize = 50,
-            //        PhotoSize = PhotoSize.MaxWidthHeight,
-            //        MaxWidthHeight = 2000
-            //    });
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+               {
+                   Name = pName,
+                   Directory = pdirectory,
+                   CompressionQuality = 75,
+                   CustomPhotoSize = 50,
+                   PhotoSize = PhotoSize.MaxWidthHeight,
+                   MaxWidthHeight = 2000
+               });
 
-            //    if (file == null)
-            //    {
-            //        //await _dialogService.ShowError("La foto no se tomo correctamente", "Error", "OK", null);
-            //        return;
-            //    }
+               if (file == null)
+               {
+                   //await _dialogService.ShowError("La foto no se tomo correctamente", "Error", "OK", null);
+                   return;
+               }
 
 
-			// 	var FacePhoto1 = pName;
-			// 	var FacePhotoPath1 = file.Path;
-			// 	var UpdateFace1 = true;
-			// 	var NewoneFace1 = false;
+				FacePhoto1 = pName;
+				PhotoPath = file.Path;
+				var UpdateFace1 = true;
+				var NewoneFace1 = false;
+				byte[] FileImage ;
+				string CodeHash = string.Empty;
+
+
+			if (PhotoPath != "")
+            {
+                var img = File.ReadAllBytes(PhotoPath);
+                SHA512 objAlgoritmo = SHA512.Create();
+                var imgHash = objAlgoritmo.ComputeHash(img);
+                FileImage = img;
+                var FileName = FacePhoto1;
+                var Face = 1;//face;
+            	CodeHash = Convert.ToBase64String(imgHash);
+            }
+            else
+            {
+                FileImage = new byte[0];
+                CodeHash = "";
+            }
   
 
                //MediaScannerConnection.ScanFile(Android.App.Application.Context, new string[] { file.Path }, null, null);

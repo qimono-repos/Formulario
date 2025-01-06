d0b073252aec82c500365d17aa32d22e3e386c38
Albert.Gruning <Alberto.Gruning@grupoeconat.com>
2025-01-06 16:20:56 -0300

    feat: add elements for file management

--- Formulario/MainPage.xaml.cs
+++ Formulario/MainPage.xaml.cs
@@ -1,4 +1,11 @@
-﻿namespace Formulario;
+﻿using System;
+// using Plugin.Media.Abstractions;
+// using Plugin.Media;
+
+//using Android.App;
+
+
+namespace Formulario;
 
 public partial class MainPage : ContentPage
 {
@@ -9,18 +16,92 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+	public async void OpenAddPhoto_Clicked(object sender, EventArgs e)
+        {
+            var b = (Button)sender;
+            // var boxPhoto = b.Parent as StackLayout;
+            // var buttonAdd = (Button)boxPhoto.Children[0];
+            // var ide = buttonAdd.ClassId;
+			var ide = b.ClassId;
 
-		if (count == 1)
-			CounterBtn.Text = $"Evento : primera vez";
-		else
-			CounterBtn.Text = $"Eventos : {count} veces";
+			//await Task.Run(()=> CounterBtn.Text = $"Clicked");
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+			//CounterBtn.Text = $"Clicked";
 
-	}
+            //await viewModel.ExecuteTakePhotoCommand();
+            //await viewModel.ExecuteTakePhotoCommand(ide);
+
+			try
+           {
+            //    //Plugin.Media.Abstractions.Location _location = await GetGeolocation();
+            //    await CrossMedia.Current.Initialize();
+
+            //    if (!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
+            //    {
+            //        await _dialogService.ShowError("Camara no detectada", "Error", "OK", null);
+            //        return;
+            //    }
+            //    var photoName = "";
+            //    var varName = "";
+
+            // 	photoName = $"PHOTO_{DateTime.Now.ToString()}";
+
+
+            //    var pName = "";
+            //    var pdirectory = $"Face_{new Random().Next(99).ToString()}_{DateTime.Now.ToString("yyyyMMddHHmmss")}.jpg";
+               
+            //    pdirectory = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+            //    var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            //    {
+            //        Name = pName,
+            //        Directory = pdirectory,
+            //        CompressionQuality = 75,
+            //        CustomPhotoSize = 50,
+            //        PhotoSize = PhotoSize.MaxWidthHeight,
+            //        MaxWidthHeight = 2000
+            //    });
+
+            //    if (file == null)
+            //    {
+            //        //await _dialogService.ShowError("La foto no se tomo correctamente", "Error", "OK", null);
+            //        return;
+            //    }
+
+
+			// 	var FacePhoto1 = pName;
+			// 	var FacePhotoPath1 = file.Path;
+			// 	var UpdateFace1 = true;
+			// 	var NewoneFace1 = false;
+  
+
+               //MediaScannerConnection.ScanFile(Android.App.Application.Context, new string[] { file.Path }, null, null);
+#if DEBUG
+				await DisplayAlert("Error", $"Fin del proceso Multimedia {ide}" , "Aceptar");
+#endif
+
+           }
+           catch (Exception ex)
+           {
+				await DisplayAlert("Error", ex.Message , "Aceptar");
+               //new DisplayInfo();// _dialogService.ShowError(ex.Message, "Error", "Aceptar", null);""
+           }
+        }
+
+
+	// private void OnCounterClicked(object sender, EventArgs e)
+	// {
+	// 	count++;
+
+	// 	if (count == 1)
+	// 		CounterBtn.Text = $"Evento : primera vez";
+	// 	else
+	// 		CounterBtn.Text = $"Eventos : {count} veces";
+
+	// 	SemanticScreenReader.Announce(CounterBtn.Text);
+
+	// }
+
 
 	private void OnCounterCompleted(object s, EventArgs e)
 	{

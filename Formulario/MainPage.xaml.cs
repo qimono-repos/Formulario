6912844e06fd7635ee3f61e71e797955ed493ea8
Albert.Gruning <Alberto.Gruning@grupoeconat.com>
2025-01-06 23:14:36 -0300

    ref: extract method to decouple local storage from image capture

--- Formulario/MainPage.xaml.cs
+++ Formulario/MainPage.xaml.cs
@@ -58,98 +58,102 @@ public partial class MainPage : ContentPage
             //await viewModel.ExecuteTakePhotoCommand(ide);
 
 			try
-           {
+        {
             //    //Plugin.Media.Abstractions.Location _location = await GetGeolocation();
-               await CrossMedia.Current.Initialize();
+            await CrossMedia.Current.Initialize();
 
-               if (!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
-               {
-					await DisplayAlert("Error", "Camara no detectada" , "Aceptar");
-                   //await _dialogService.ShowError("Camara no detectada", "Error", "OK", null);
-                   return;
-               }
-               var photoName = "";
-               var varName = "";
-
-            	photoName = $"PHOTO_{DateTime.Now.ToString()}";
-
-
-               	var pName = "";
-            	var pdirectory = $"Face_{new Random().Next(99).ToString()}_{DateTime.Now.ToString("yyyyMMddHHmmss")}.jpg";
-               
-                pdirectory = DateTime.Now.ToString("yyyyMMddHHmmss");
-
-                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
-               {
-                   Name = pName,
-                   Directory = pdirectory,
-                   CompressionQuality = 75,
-                   CustomPhotoSize = 50,
-                   PhotoSize = PhotoSize.MaxWidthHeight,
-                   MaxWidthHeight = 2000
-               });
-
-               if (file == null)
-               {
-                   //await _dialogService.ShowError("La foto no se tomo correctamente", "Error", "OK", null);
-                   return;
-               }
-
-
-				FacePhoto1 = pName;
-				PhotoPath = file.Path;
-				var UpdateFace1 = true;
-				var NewoneFace1 = false;
-				byte[] FileImage ;
-				string CodeHash = string.Empty;
-
-
-			if (PhotoPath != "")
+            if (!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
             {
-                var img = File.ReadAllBytes(PhotoPath);
-                SHA512 objAlgoritmo = SHA512.Create();
-                var imgHash = objAlgoritmo.ComputeHash(img);
-                FileImage = img;
-                var FileName = FacePhoto1;
-                var Face = 1;//face;
-            	CodeHash = Convert.ToBase64String(imgHash);
+                await DisplayAlert("Error", "Camara no detectada", "Aceptar");
+                //await _dialogService.ShowError("Camara no detectada", "Error", "OK", null);
+                return;
             }
-            else
-            {
-                FileImage = new byte[0];
-                CodeHash = "";
-            }
-  
+            var photoName = "";
+            var varName = "";
 
-               //MediaScannerConnection.ScanFile(Android.App.Application.Context, new string[] { file.Path }, null, null);
+            photoName = $"PHOTO_{DateTime.Now.ToString()}";
+
+
+            var pName = "";
+            var pdirectory = $"Face_{new Random().Next(99).ToString()}_{DateTime.Now.ToString("yyyyMMddHHmmss")}.jpg";
+
+            pdirectory = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Name = pName,
+                Directory = pdirectory,
+                CompressionQuality = 75,
+                CustomPhotoSize = 50,
+                PhotoSize = PhotoSize.MaxWidthHeight,
+                MaxWidthHeight = 2000
+            });
+
+            if (file == null)
+            {
+                //await _dialogService.ShowError("La foto no se tomo correctamente", "Error", "OK", null);
+                return;
+            }
+
+
+            FacePhoto1 = pName;
+            PhotoPath = file.Path;
+            var UpdateFace1 = true;
+            var NewoneFace1 = false;
+            byte[] FileImage;
+            string CodeHash = string.Empty;
+
+    		//SaveFile(out FileImage, out CodeHash);
+
+
+            //MediaScannerConnection.ScanFile(Android.App.Application.Context, new string[] { file.Path }, null, null);
 #if DEBUG
-				await DisplayAlert("Error", $"Fin del proceso Multimedia {ide}" , "Aceptar");
+            await DisplayAlert("Error", $"Fin del proceso Multimedia {ide}", "Aceptar");
 #endif
 
-           }
-           catch (Exception ex)
+        }
+        catch (Exception ex)
            {
 				await DisplayAlert("Error", ex.Message , "Aceptar");
                //new DisplayInfo();// _dialogService.ShowError(ex.Message, "Error", "Aceptar", null);""
            }
         }
 
+    private void SaveFile(out byte[] FileImage, out string CodeHash)
+    {
+        if (PhotoPath != "")
+        {
+            var img = File.ReadAllBytes(PhotoPath);
+            SHA512 objAlgoritmo = SHA512.Create();
+            var imgHash = objAlgoritmo.ComputeHash(img);
+            FileImage = img;
+            var FileName = FacePhoto1;
+            var Face = 1;//face;
+            CodeHash = Convert.ToBase64String(imgHash);
+        }
+        else
+        {
+            FileImage = new byte[0];
+            CodeHash = "";
+        }
+    }
 
-	// private void OnCounterClicked(object sender, EventArgs e)
-	// {
-	// 	count++;
 
-	// 	if (count == 1)
-	// 		CounterBtn.Text = $"Evento : primera vez";
-	// 	else
-	// 		CounterBtn.Text = $"Eventos : {count} veces";
+    // private void OnCounterClicked(object sender, EventArgs e)
+    // {
+    // 	count++;
 
-	// 	SemanticScreenReader.Announce(CounterBtn.Text);
+    // 	if (count == 1)
+    // 		CounterBtn.Text = $"Evento : primera vez";
+    // 	else
+    // 		CounterBtn.Text = $"Eventos : {count} veces";
 
-	// }
+    // 	SemanticScreenReader.Announce(CounterBtn.Text);
+
+    // }
 
 
-	private void OnCounterCompleted(object s, EventArgs e)
+    private void OnCounterCompleted(object s, EventArgs e)
 	{
 		CounterBtn.Text = $"Completado";
 	}
